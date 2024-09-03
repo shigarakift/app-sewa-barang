1da5f86fdd4b2ae10f0bd41582987bb104570b54
BagasAlfatah <bagasritonga89@gmail.com>
2024-09-03 21:26:27 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'category_id',
+        'brand_id',
+        'price',
+
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function photos():HasMany
+    {
+        return $this->hasMany(ProductPhoto::class);
+    }
+}
